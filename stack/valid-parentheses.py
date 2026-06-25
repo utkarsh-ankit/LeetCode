3151f887781f3dabaface491e53ad2d7ddb8e6b0
utk <utkarsh.ankit09@gmail.com>
2026-06-25 03:07:55 -0700

    LC-valid-parentheses [Runtime: N/A; Memory: N/A]

---
+++ stack/valid-parentheses.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        a={"}":"{", "]":"[", ")":"("}
+        stack=[]
+
+        for i in s:
+            if i in ("{", "(", "["):
+                stack.append(i)
+            elif i in a and stack and a[i]==stack[-1]:
+                stack.pop()
+            else:
+                return False
+        return True if stack else False
+
+
+        
