df505535d3a5b61b9967e15a19b2570600a2a1ae
utk <utkarsh.ankit09@gmail.com>
2026-06-24 23:03:28 -0700

    LC-reorganize-string [Runtime: 3 ms; Memory: 19.3 MB]

--- heap-priority-queue/reorganize-string.py
+++ heap-priority-queue/reorganize-string.py
@@ -1,34 +1,57 @@
-import heapq
 class Solution:
-    def reorganizeString(self, s: str) -> str:
-        t=[]
-        y={i:s.count(i) for i in s}
-        if max(y.values()) > (sum(y.values())-max(y.values()))+1:
+    def reorganizeString(self, s:str)->str:
+        # cnt = Counter(s)
+        # maxH = [(-c, ch) for ch, c in cnt.items()]
+        # heapq.heapify(maxH)
+        # prev = None
+        # res = ''
+
+        # while prev or maxH:
+        #     if prev and not maxH:
+        #         return ''
+
+        #     c, ch = heapq.heappop(maxH)
+        #     res += ch
+        #     c += 1
+
+        #     if prev:
+        #         heapq.heappush(maxH, prev)
+        #         prev = None
+        #     if c!=0:
+        #         prev = (c, ch)
+
+        # return res
+
+        count=Counter(s)
+
+        if max(count.values())>((len(s)+1)//2):
             return ""
-
-        for char, freq in y.items():
-            heapq.heappush(t, (-freq, char))
-
-        result = []
-        prev_freq, prev_char = 0, ""
-
-        while t:
-            freq, char = heapq.heappop(t)
-            result.append(char)
-
-            if prev_freq<0:
-                heapq.heappush(t, (prev_freq, prev_char))
-
-            prev_freq=freq+1
-            prev_char=char
-
-        return "".join(result)
-
-            
-
-
-
-
-
-            
         
+        max_count=[(-co, ch) for ch, co in count.items()]
+
+        heapq.heapify(max_count)
+
+        string=""
+        prev=None
+
+        while max_count:
+            co,ch= heapq.heappop(max_count)
+            
+            if ch==prev:
+                co2,ch2= heapq.heappop(max_count)
+                string+=ch2
+                co2+=1
+                if co2!=0:
+                    heapq.heappush(max_count, (co2,ch2))
+                heapq.heappush(max_count, (co,ch))
+            else:
+                string+=ch
+                co+=1
+                if co!=0:
+                    heapq.heappush(max_count, (co,ch))
+            prev=string[-1]
+
+        return string
+
+
+
