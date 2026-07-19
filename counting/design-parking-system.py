57d2cac599457e33c53dcc728d1ab14460f2e851
utk <utkarsh.ankit09@gmail.com>
2026-07-18 22:35:27 -0700

    LC-design-parking-system [Runtime: N/A; Memory: N/A]

--- counting/design-parking-system.py
+++ counting/design-parking-system.py
@@ -1,19 +1,16 @@
 class ParkingSystem:
 
     def __init__(self, big: int, medium: int, small: int):
-        self.big, self.medium, self.small = big, medium, small
-        self.map ={
-            1: self.big,
-            2: self.medium,
-            3: self.small
-        }
+        self.space={1:big,
+        2:medium,
+        3:small}
 
     def addCar(self, carType: int) -> bool:
-        self.map[carType] -= 1
-        if self.map[carType] <0:
-            self.map[carType] = 0
-            return False
-        return True
+        if self.space[carType]>0:
+            self.space[carType]-=1
+            return True
+        return False
+
 
 # Your ParkingSystem object will be instantiated and called as such:
 # obj = ParkingSystem(big, medium, small)
