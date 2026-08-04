5449b624c0e6e733f2018b32e74b2db9188224b2
utk <utkarsh.ankit09@gmail.com>
2026-08-03 17:26:39 -0700

    LC-minimum-area-rectangle [Runtime: N/A; Memory: N/A]

--- sorting/minimum-area-rectangle.py
+++ sorting/minimum-area-rectangle.py
@@ -6,6 +6,23 @@ class Solution:
 
         # if we have those 2 points exist on our hashset. If yes, we calcualte the area and track the minimum.
 
+        # hmap={tuple(i) for i in points}
+        # area=float("inf")
+
+        # for i in range(len(points)):
+        #     for j in range(i+1, len(points)):
+        #         x1,y1=points[i]
+        #         x2,y2=points[j]
+
+        #         if x1==x2 or y1==y2:
+        #             continue
+                
+        #         if (x1,y2) in hmap and (x2,y1) in hmap:
+        #             area= min(area, abs(x2-x1)*abs(y2-y1))
+
+        # return area if area!=float("inf") else 0
+
+
         hmap={tuple(i) for i in points}
         area=float("inf")
 
@@ -18,7 +35,7 @@ class Solution:
                     continue
                 
                 if (x1,y2) in hmap and (x2,y1) in hmap:
-                    area= min(area, abs(x2-x1)*abs(y2-y1))
+                    area=min(area, abs(x2-x1)*abs(y2-y1))
 
         return area if area!=float("inf") else 0
 
