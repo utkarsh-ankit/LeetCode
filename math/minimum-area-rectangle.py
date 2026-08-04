ec5b45ea310fe956393eedf2d79e8eeedbe842f9
utk <utkarsh.ankit09@gmail.com>
2026-08-03 18:13:04 -0700

    LC-minimum-area-rectangle [Runtime: N/A; Memory: N/A]

--- math/minimum-area-rectangle.py
+++ math/minimum-area-rectangle.py
@@ -37,7 +37,7 @@ class Solution:
                 if (x1,y2) in hmap and (x2,y1) in hmap:
                     area=min(area,abs(x2-x1)*abs(y2-y1))
 
-        return area if area!=float("inf") else -1
+        return area if area!=float("inf") else 0
 
 
 
